ae89d06582bf996aa96257291696ca7466c71be8
Nageeb Zafar <75119519+ZafarNageeb@users.noreply.github.com>
2021-10-05 10:03:02 +0200

    Made Code better

--- InfiniMessage.py
+++ InfiniMessage.py
@@ -2,16 +2,22 @@ import pyautogui
 import time
 import tkinter as tk
 
-tijd = float(input("Input an input interval in seconds: "))
-iteration = int(input("How often do you want it to print: "))
-stringToPrint = (input("What message do you want to print: "))
-time.sleep(5)
-counter = 0
 
-while counter < iteration:
-    time.sleep(tijd)
-    pyautogui.typewrite(stringToPrint)
-    pyautogui.press('enter')
-    counter += 1
-print("Program Finished")
-raise SystemExit
+def main():
+    tijd = float(input("Input an input interval in seconds: "))
+    iteration = int(input("How often do you want it to print: "))
+    stringToPrint = (input("What message do you want to print: "))
+    time.sleep(5)
+    counter = 0
+
+    while counter < iteration:
+        time.sleep(tijd)
+        pyautogui.typewrite(stringToPrint)
+        pyautogui.press('enter')
+        counter += 1
+    print("Program Finished")
+    raise SystemExit
+
+
+if __name__ == '__main__':
+    main()
