8910418f84992269c430fed49f61fa77ed52b90c
Nageeb Zafar <75119519+ZafarNageeb@users.noreply.github.com>
2021-11-22 15:20:33 +0100

    Added Feature and renaming
    Added an number iterating script for repeated increased number typing
    
    Also renamed some variables

--- InfiniMessageNumbers.py
+++ InfiniMessageNumbers.py
@@ -6,14 +6,16 @@ import pyautogui
 def main():
     time_between_messages = float(input("Input an input interval in seconds: "))
     iteration = int(input("How often do you want it to print: "))
-    string_to_print = (input("What message do you want to print: "))
+    starting_number = (input("What will be the first number to print: "))
+    number = int(starting_number)
     time.sleep(5)
     counter = 0
 
     while counter < iteration:
         time.sleep(time_between_messages)
-        pyautogui.typewrite(string_to_print)
+        pyautogui.typewrite(str(number))
         pyautogui.press('enter')
+        number += 1
         counter += 1
     print("Program Finished")
     raise SystemExit
